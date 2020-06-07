c32b2e127421bf70db0e606b5feb265aad0681bf
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-06 18:33:07 -0600

    Retrieves geocode info within spec file

--- spec/requests/api/v1/google_coord_req_spec.rb
+++ spec/requests/api/v1/google_coord_req_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 describe 'Google Geocoding API' do
   it 'can return lat and long for a given city' do
     location = 'Denver, CO'
-    # expected = {"lat" : 39.91424689999999, "lng" : -104.6002959}
+    expected = {:lat=>39.7392358, :lng=>-104.990251}
     json_resp = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?address=denver, co&key=AIzaSyBSh0Z_46DOE6XjhwOT3BCS4E8-qctg2dg")
-  binding.pry
+    parsed = JSON.parse(json_resp.body, symbolize_names: true)
+  
+    expect(parsed[:results][0][:geometry][:location]).to eq(expected)
   end
 end
