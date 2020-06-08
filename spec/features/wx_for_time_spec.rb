f02a88fa0a7b47abe1f367389d291f3ac9fe9331
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-08 10:30:57 -0600

    Researching Zomato API

--- spec/features/wx_for_time_spec.rb
+++ spec/features/wx_for_time_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 describe 'OpenWeather API' do
-  it 'can return weather for given time' do
+  xit 'can return weather for given time' do
     # travel_time = '1 hour 48 mins'
     location = 'pueblo, co'
     unix_time = 1591653600
     # for 06/08/2020 4 PM local Denver
   
     wx = DestinationForecast.arrival_wx(location, unix_time)
-    # expect(time[:routes].first[:legs].first[:duration][:text]).to be_a(String)
   end
 end
