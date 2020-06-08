df6ca37c3e287ee171554fad8d9c871c427aabad
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-08 10:15:13 -0600

    Rethinking time weather -- saw we can use current forecast

---
+++ app/poros/destination_forecast.rb
@@ -0,0 +1,8 @@
+class DestinationForecast
+
+  def self.arrival_wx(location, unix_time)
+    lat_long = 
+    binding.pry
+  end
+
+end
--- spec/features/wx_for_time_spec.rb
+++ spec/features/wx_for_time_spec.rb
@@ -4,9 +4,10 @@ describe 'OpenWeather API' do
   it 'can return weather for given time' do
     # travel_time = '1 hour 48 mins'
     location = 'pueblo, co'
-    clock_time = "??"
+    unix_time = 1591653600
+    # for 06/08/2020 4 PM local Denver
   
-    wx = DirectionsService.travel_time(start, finish)
-    expect(time[:routes].first[:legs].first[:duration][:text]).to be_a(String)
+    wx = DestinationForecast.arrival_wx(location, unix_time)
+    # expect(time[:routes].first[:legs].first[:duration][:text]).to be_a(String)
   end
 end
