815f5389ea569af6e466465ceeb79f54f43e334a
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-07 21:27:09 -0600

    Replaces hardcoded location with variable value

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,7 +1,7 @@
 class GeocodeService
 
     def self.get_geocode(location)
-      json_resp = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?address=denver, co") do |req|
+      json_resp = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{location}") do |req|
         req.params['key'] = ENV['GOOG_GEO_API']
       end
       JSON.parse(json_resp.body, symbolize_names: true)
