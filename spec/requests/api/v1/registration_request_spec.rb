aaa93e81ccc7e2213245ad700571e708e276a68d
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-10 05:55:29 -0600

    Returns error if user tries to create with existing email

--- spec/requests/api/v1/registration_request_spec.rb
+++ spec/requests/api/v1/registration_request_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe 'Registration Request' do
-  before(:each) do
-    User.create(email: "alreadyhere@example.com",
-                       password: "snoopy")
-  end
+  # before(:each) do
+  #   User.create(email: "alreadyhere@example.com",
+  #                      password: "snoopy")
+  # end
   
   it 'can create a user and return unique assigned api key' do
     body = {
@@ -24,37 +24,28 @@ describe 'Registration Request' do
     expect(json[:data]).to have_key(:id)
     expect(json[:data][:attributes]).to have_key(:email)
     expect(json[:data][:attributes]).to have_key(:api_key)
+    expect(json[:data][:attributes][:api_key].length).to eq(27)
     expect(json[:data][:attributes]).not_to have_key(:password)
   end
   
-  xit 'returns error status and description for invalid email' do
+  it 'returns error status and description for invalid email' do
+    User.create(email: "alreadyhere@example.com",
+                password: "snoopy")
+    
     reg_params = {
                   "email": "alreadyhere@example.com",
                   "password": "password",
                   "password_confirmation": "password"
                   }
 
-    post '/api/v1/sessions', params: reg_params
+    post '/api/v1/users', params: reg_params
 
     expect(response).not_to be_successful
     expect(response.status).to eq(401)
 
     json = JSON.parse(response.body, symbolize_names: true )
+    
     expect(json[:error]).to eq("credentials are bad")
     expect(json[:status]).to eq(401)
   end
 end
-
-# status: 201
-# body:
-
-# {
-#   "data": {
-#     "type": "users",
-#     "id": "1",
-#     "attributes": {
-#       "email": "whatever@example.com",
-#       "api_key": "jgn983hy48thw9begh98h4539h4"
-#     }
-#   }
-# }
