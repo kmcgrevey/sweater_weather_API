dc759494af6d5901fd66761f5fec1fb364cf70d9
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-09 19:33:13 -0600

    Returns login json response

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,32 @@
 class Api::V1::SessionsController < ApplicationController
 
    def create
-  
-  end
+    user = User.find_by(email: params[:email])
+    # binding.pry
+    
+    # if user.authenticate(params[:password])
+    if user.password == params[:password]
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user), status: 200
 
-   
+    #   flash[:success] = "You are logged in!"
+    #   if user.default?
+    #     redirect_to "/profile"
+    #   elsif user.merchant?
+    #     redirect_to "/merchant"
+    #   elsif user.admin?
+    #     redirect_to "/admin"
+    #   end
+    else
+      render status: 201#, "credentials are bad" 
+    #   flash[:error] = "The credentials you entered are incorrect"
+    #   redirect_to "/login"
+    end
+
+    # private
+
+    # def login_params
+    #   params.permit(:email)
+  end
 
 end
--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe 'Login Request' do
   it 'returns a users api key' do
+    user = User.create(email: "whatever@example.com",
+                       password: "password")
+    
     login_params = {
                     "email": "whatever@example.com",
                     "password": "password"
@@ -11,6 +14,14 @@ describe 'Login Request' do
     post '/api/v1/sessions', params: login_params
 
     expect(response).to be_successful
+    expect(response.status).to eq(200)
+  
+    json = JSON.parse(response.body, symbolize_names: true )
+    
+    expect(json[:data][:type]).to eq("user")
+    expect(json[:data]).to have_key(:id)
+    expect(json[:data][:attributes]).to have_key(:email)
+    expect(json[:data][:attributes]).to have_key(:api_key)
 
   end
 end
