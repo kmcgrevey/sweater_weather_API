3771ccd08677e61c68d01ba7d438127ddfdc88d5
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-10 00:41:08 -0600

    Starts test for invalid email during registration

--- spec/requests/api/v1/registration_request_spec.rb
+++ spec/requests/api/v1/registration_request_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe 'Registration Request' do
+  before(:each) do
+    User.create(email: "alreadyhere@example.com",
+                       password: "snoopy")
+  end
+  
   it 'can create a user and return unique assigned api key' do
     body = {
             "email": "whatever@example.com",
@@ -20,6 +25,23 @@ describe 'Registration Request' do
     expect(json[:data][:attributes]).to have_key(:email)
     expect(json[:data][:attributes]).to have_key(:api_key)
     expect(json[:data][:attributes]).not_to have_key(:password)
+  end
+  
+  xit 'returns error status and description for invalid email' do
+    reg_params = {
+                  "email": "alreadyhere@example.com",
+                  "password": "password",
+                  "password_confirmation": "password"
+                  }
+
+    post '/api/v1/sessions', params: reg_params
+
+    expect(response).not_to be_successful
+    expect(response.status).to eq(401)
+
+    json = JSON.parse(response.body, symbolize_names: true )
+    expect(json[:error]).to eq("credentials are bad")
+    expect(json[:status]).to eq(401)
   end
 end
 
