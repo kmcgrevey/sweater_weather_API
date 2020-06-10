a1511a6a9b6d95788142c4a53f171efc218b3daf
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-09 23:59:48 -0600

    Returns error code and message for invalid api key

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::RoadtripController < ApplicationController
 
   def create
-    # binding.pry
-    roadtrip = RoadtripSearch.make_trip(origin, destination)
-    render json: RoadtripSerializer.new(roadtrip)
+    user = User.find_by(api_key: user_api_key)
+   
+    if user
+      roadtrip = RoadtripSearch.make_trip(origin, destination)
+      render json: RoadtripSerializer.new(roadtrip)
+    else
+      render json: {error: "Unauthorized",
+                    status: 401},
+                    status: 401
+    end
   end
 
   private
@@ -14,6 +21,10 @@ class Api::V1::RoadtripController < ApplicationController
   
   def destination
     params[:destination]
+  end
+  
+  def user_api_key
+    params[:api_key]
   end
 
 end
